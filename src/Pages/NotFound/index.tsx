7bbbf4dcdbcd7cf9c7a2d63541baefc28b9b893b
andersonrugeles <lrujeles@pratechgroup.com>
2024-08-09 09:27:26 -0500

    Update page notfound

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -1,10 +1,21 @@
-function NotFound() {
+import { Link } from 'react-router-dom';
 
-    return (
-      <div>
-        <h1 className="bg-red-100">Hello world!</h1>
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+      <div className="bg-red-100 border border-red-300 text-red-800 px-4 py-2 rounded-lg shadow-md">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-xl mt-2">¡Página no encontrada!</p>
+        <p className="mt-4">Lo sentimos, la página que buscas no existe.</p>
+        <Link
+          to="/"
+          className="mt-6 inline-block px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        >
+          Volver al inicio
+        </Link>
       </div>
-    );
-  }
-  
-  export default NotFound
+    </div>
+  );
+};
+
+export default NotFound;
